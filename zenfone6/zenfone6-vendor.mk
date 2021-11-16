2b4dfd64cd8b54fe0e1cda12fc4b0b584999d134
dmd79 <davidevinavil@gmail.com>
2021-11-16 01:33:42 +0100

    zenfone6: Add libaptX
    11-16 01:14:58.506  2340  2340 E a2dp_vendor_aptx_encoder: system/bt/stack/a2dp/a2dp_vendor_aptx_encoder.cc:123 A2DP_VendorLoadEncoderAptx: A2DP_VendorLoadEncoderAptx: cannot open aptX encoder library libaptX_encoder.so: dlopen failed: library "libaptX_encoder.so" not found
    11-16 01:14:58.506  2340  2340 E a2dp_vendor_aptx: system/bt/stack/a2dp/a2dp_vendor_aptx.cc:465 init: init: cannot load the encoder
    11-16 01:14:58.506  2340  2340 E a2dp_vendor_aptx_hd_encoder: system/bt/stack/a2dp/a2dp_vendor_aptx_hd_encoder.cc:123 A2DP_VendorLoadEncoderAptxHd: A2DP_VendorLoadEncoderAptxHd: cannot open aptX-HD encoder library libaptXHD_encoder.so: dlopen failed: library "libaptXHD_encoder.so" not found
    11-16 01:14:58.506  2340  2340 E a2dp_vendor_aptx_hd: system/bt/stack/a2dp/a2dp_vendor_aptx_hd.cc:483 init: init: cannot load the encoder

--- zenfone6/zenfone6-vendor.mk
+++ zenfone6/zenfone6-vendor.mk
@@ -28,6 +28,8 @@ PRODUCT_COPY_FILES += \
     vendor/asus/zenfone6/proprietary/lib/DataSet/ispDB/ASUS_UNDI.bin:$(TARGET_COPY_OUT_SYSTEM)/lib/DataSet/ispDB/ASUS_UNDI.bin \
     vendor/asus/zenfone6/proprietary/lib/DataSet/ispDB/ASUS_UNDI_ZOOM.bin:$(TARGET_COPY_OUT_SYSTEM)/lib/DataSet/ispDB/ASUS_UNDI_ZOOM.bin \
     vendor/asus/zenfone6/proprietary/lib/DataSet/ispDB/ParameterDB.db:$(TARGET_COPY_OUT_SYSTEM)/lib/DataSet/ispDB/ParameterDB.db \
+    vendor/asus/zenfone6/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so \
+    vendor/asus/zenfone6/proprietary/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptXHD_encoder.so \
     vendor/asus/zenfone6/proprietary/priv-app/AsusCamera/lib/arm64/libUndistort.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/AsusCamera/lib/arm64/libUndistort.so \
     vendor/asus/zenfone6/proprietary/priv-app/AsusCamera/lib/arm64/libarcsoft_distortion_correction.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/AsusCamera/lib/arm64/libarcsoft_distortion_correction.so \
     vendor/asus/zenfone6/proprietary/priv-app/AsusCamera/lib/arm64/libarcsoft_object_tracking.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/AsusCamera/lib/arm64/libarcsoft_object_tracking.so \
